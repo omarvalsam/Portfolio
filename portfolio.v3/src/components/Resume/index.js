e664073dae21ed999e4363f1bc0bbb7110d1b1c6
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-04-10 21:46:06 -0600

    completed react portfolio and also resume component

--- portfolio.v3/src/components/Resume/index.js
+++ portfolio.v3/src/components/Resume/index.js
@@ -1,7 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
+import me from "../../assets/images/updatedpic.png";
 
 const Resume = () => {
-  return <div>HELLO WORLD</div>;
+  return (
+    <div className="resume">
+      <img src={me} className="logo" alt="Omars logo" />
+      <div>
+        If you're interested in learning a bit more about my previous employers
+        or experience before coding. Feel free to download my resume.
+      </div>
+      <a
+        href="https://docs.google.com/document/d/1W0226Iiam12P6clAGN2ribSU0LTvzJgTgKyhijnw5Qs/edit?usp=sharing"
+        className="fa-solid socialmt fa-file-arrow-down"
+      />
+    </div>
+  );
 };
 
 export default Resume;
